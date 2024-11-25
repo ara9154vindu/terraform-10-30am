7020926ed962eedbfe4125652e1ed2aa3e700bf1
Aravind <aravindcheggoju@gmail.com>
2024-11-25 16:30:56 +0530

    lifecycles

---
+++ day-5-lifecycle/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "name" {
+  ami = "ami-0614680123427b75e"
+  instance_type = "t2.micro"
+  key_name = "mumbai"
+#   lifecycle {
+#    prevent_destroy = true
+#   }
+#   lifecycle {
+#    create_before_destroy = true
+#   }
+#   lifecycle {
+#    ignore_changes = [ tags ]
+#   }
+
+}
