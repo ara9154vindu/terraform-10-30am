0746e41c7908a3f9b69c8e7291d1a7efd78c43f2
Aravind <aravindcheggoju@gmail.com>
2024-11-25 15:50:03 +0530

    import

--- day-5-terraform_import/main.tf
+++ day-5-terraform_import/main.tf
@@ -1,6 +1,5 @@
-
-resource "aws_instance" "myvm" {
- ami           = "unknown"#(we need to add from state file reference)
- instance_type = "unknown"#(we need to add from state file reference)
- #rest all attributes need to be added to match actual resource preseence in AWS 
+resource "aws_instance" "vindu" {
+  ami = "ami-0614680123427b75e"
+  instance_type = "t2.micro"
+  key_name = "mumbai"
 }
